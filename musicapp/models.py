dba071fc05d2452944eb4b66e2f6672ebfbad893
trust <trustadekoye@gmail.com>
2022-11-04 11:42:21 +0100

    update

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste (models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+
+class Song (models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.PROTECT)
+    title = models.CharField(max_length=400)
+    date_released = models.DateTimeField()
+    likes = models.CharField(max_length=400)
+    artiste_id = models.CharField(max_length=400)
+
+class Lyric (models.Model):
+    content = models.TextField()
+    song_id = models.CharField(max_length=400)
+    Song = models.ForeignKey(Song, on_delete=models.PROTECT)
