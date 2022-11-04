978ee780a531a5d9cecbb08879caa97ee4f75bb0
trust <trustadekoye@gmail.com>
2022-11-04 17:35:47 +0100

    latest commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,21 +1,29 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Artiste (models.Model):
-    first_name = models.CharField(max_length=400)
-    last_name = models.CharField(max_length=400)
-    age = models.IntegerField()
+    first_name = models.CharField(max_length=400, default = "Enter First Name")
+    last_name = models.CharField(max_length=400, default = "Enter Last Name")
+    age = models.IntegerField(default = 1)
 
+    def __str__(self):
+        return self.first_name +  ' ' + self.last_name
 
 class Song (models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.PROTECT)
-    title = models.CharField(max_length=400)
-    date_released = models.DateTimeField()
-    likes = models.CharField(max_length=400)
-    artiste_id = models.CharField(max_length=400)
+    Artiste = models.ForeignKey(Artiste, on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=400, default = "Song Title")
+    date_released = models.DateTimeField(default = datetime.now())
+    likes = models.CharField(max_length=400, default = "DEFAULT VALUE")
+    artiste_identity = models.CharField(max_length=400, default = "")
+
+    def __str__(self):
+        return self.title
 
 class Lyric (models.Model):
-    content = models.TextField()
-    song_id = models.CharField(max_length=400)
-    Song = models.ForeignKey(Song, on_delete=models.PROTECT)
+    content = models.TextField(default = "Content")
+    lyrics_title = models.CharField(max_length=200, default = "")
+    Song = models.ForeignKey(Song, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.lyrics_title
